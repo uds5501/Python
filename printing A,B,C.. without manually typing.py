6c4364f4197171dfdd5ebd447d73f55a83dd630a
Uddeshya Singh <singhuddeshya5501@gmail.com>
2017-08-01 15:17:30 +0530

    Create printing A,B,C.. without manually typing.py
    A program to print all the alphabets in upper case and storing them in a list without manual typing.

---
+++ printing A,B,C.. without manually typing.py
@@ -0,0 +1,17 @@
+#Author: Uddeshya Singh
+#A program to print all the alphabets in upper case and storing them in a list without manual typing
+
+import string
+
+main_upper_arr=list(string.ascii_uppercase) #For upper case alphabets
+main_lower_arr=list(string.ascii_lowercase) #For lower case alphabets
+
+for i in main_upper_arr:   #For printing in python 3.x
+	print (i),
+	
+for i in main_upper_arr:	#For printing  in python 2.x
+	print i,				
+	
+	
+#More such examples can be included for beginners in python so that they won't waste 5 minutes on finding such stuff for school projects.
+#agreed?
